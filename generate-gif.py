31f76697e15d5afb4cc0a183011f0a06d13f959a
anthonypoon12 <anthony@poons.us>
2024-01-27 15:56:40 -0500

    change video limit to 3

--- generate-gif.py
+++ generate-gif.py
@@ -8,11 +8,13 @@ load_dotenv()
 
 API_KEY = os.getenv('GIPHY_API_KEY')
 query = "obama happy" + " no text"
+limit = 3
+outputPaths = []
 
 parameters = {
     "api_key": API_KEY,
     "q": query,
-    "limit": 1,
+    "limit": limit,
     "offset": 0,
     "rating": "g",
     "lang": "en",
@@ -21,6 +23,9 @@ parameters = {
 
 response = requests.get("https://api.giphy.com/v1/gifs/search", params=parameters)
 
-mp4url = response.json()['data'][0]['images']['original']['mp4']
+for i in range(3):
+    # Appends url to list and retrieves the video
+    mp4url = response.json()['data'][i]['images']['original']['mp4']
+    outputPaths.append(f'./tempvid{i}.mp4')
+    urllib.request.urlretrieve(mp4url, outputPaths[-1])
 
-urllib.request.urlretrieve(mp4url, './tempvid.mp4')
